f220b32f85806f62365891c41abd013a61ea9c03
panatyuki <admiral.dancho@gmail.com>
2023-12-01 13:54:10 +0700

    feat:add complete exercise

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,16 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
 let minNumber;
-// Start coding here
+
+// ข้อนี้ผมทำโดยใช้ Chat GPT ช่วยครับ //
+
+minNumber = 0;
+minNumber = numbers[0];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < minNumber) {
+    minNumber = Math.min(numbers[i]);
+  }
+}
 
 console.log(minNumber);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,10 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+// ข้อนี้ผมทำโดยใช้ Chat GPT ช่วยครับ //
+
+for (let i = companyName.length - 1; i >= 0; i--) {
+  reversedCompanyName = reversedCompanyName + companyName[i];
+}
 
 console.log(reversedCompanyName);
